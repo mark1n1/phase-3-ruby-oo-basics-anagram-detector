aaf9a57dd689419b6f8e7a2fb608ae2afa61c89e
Marcos Valderrabano <ing.marco.valu@gmail.com>
2022-07-17 13:31:10 -0700

    Completed bonus lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    return_array = []
+    array.map do |single_word|
+      if single_word.chars.sort == @word.chars.sort
+        return_array << single_word
+      end
+    end
+    return_array
+  end
+end
